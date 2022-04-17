8bcd6d3748986fc70031ef0cca0adce8d1112c05
EvaGurtovaia <eva.gurtovaya@gmail.com>
2022-04-17 14:32:22 -0500

    User choice and computer choice outputs are working

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
-const computerChoice = document.getElementById("computer-choice");
-const yourChoice = document.getElementById("user-choice");
+const computerChoiceDisplay = document.getElementById("computer-choice");
+const yourChoiceDisplay = document.getElementById("user-choice");
 const result = document.getElementById("result");
 const possibleChoices = document.querySelectorAll("button");
-let userChoice;
+let yourChoice;
+let computerChoice;
 
 possibleChoices.forEach((possibleChoice) => {
     possibleChoice.addEventListener("click", (e) => {
-        userChoice = e.target.dispatchEvent;
+        yourChoice = e.target.id;
+        yourChoiceDisplay.textContent = yourChoice;
+        generateComputerChoice();
     });
 });
+
+function generateComputerChoice() {
+    const randomNum = Math.floor(Math.random() * 3);
+    if (randomNum === 0) {
+        computerChoice = "rock";
+    }
+    if (randomNum === 1) {
+        computerChoice = "paper";
+    }
+    if (randomNum === 2) {
+        computerChoice = "scissors";
+    }
+    computerChoiceDisplay.innerHTML = computerChoice;
+}
