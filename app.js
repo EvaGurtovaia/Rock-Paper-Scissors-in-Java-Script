f197fd62ec9045f896ead75e1a3b12bc207bb654
EvaGurtovaia <eva.gurtovaya@gmail.com>
2022-04-17 13:51:05 -0500

    Adding some Java Script logic

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
+const computerChoice = document.getElementById("computer-choice");
+const yourChoice = document.getElementById("user-choice");
+const result = document.getElementById("result");
+const possibleChoices = document.querySelectorAll("button");
+let userChoice;
 
+possibleChoices.forEach((possibleChoice) => {
+    possibleChoice.addEventListener("click", (e) => {
+        userChoice = e.target.dispatchEvent;
+    });
+});
